5eb78e6407d391915ac53ed9f5809a4ece3b7dde
gsrcodes <girdharsinghrajpurohit353@gmail.com>
2025-10-07 22:09:31 +0530

    Add files via upload

---
+++ Message/Consumer.java
@@ -0,0 +1,16 @@
+package message;
+class Consumer extends Thread {
+    private MessageBoard board;
+
+    public Consumer(MessageBoard b) {
+        this.board = b;
+    }
+
+    @Override
+    public void run() {
+        String msg;
+        while (!(msg = board.get()).equals("DONE")) {
+            System.out.println("Consumed: " + msg);
+        }
+    }
+}
